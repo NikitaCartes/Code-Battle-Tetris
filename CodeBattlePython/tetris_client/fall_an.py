11c5f968f3362a4e64b0bc586b13255640709d0e
AstralRomance <andrdoncwork@mail.ru>
2020-12-04 22:02:43 +0300

    collision analysis

---
+++ CodeBattlePython/tetris_client/fall_an.py
@@ -0,0 +1,22 @@
+from tetris_client import Board, Element, Point
+
+
+def table_after_fall(figure_coors: Element, current_board: list) -> list:
+    collision = False
+    collision_points = []
+    new_board = current_board.copy()
+    for pnt in figure_coors:
+        if Point(pnt.get_x(), pnt.get_y()-1) in figure_coors:
+            continue
+        else:
+            collision_points.append(Point(pnt.get_x(), pnt.get_y()))
+    delta = 1
+    while collision == False:
+        shifted_points = [Point(pnt.get_x, pnt.get_y()-delta) for pnt in collision_points]
+        for pnt in shifted_points:
+            if (pnt.get_x(), pnt.get_y()) in current_board:
+                collision = True
+                new_board.append((Point(pnt.get_x(), pnt.get_y()-delta+1 for pnt in figure_coors)))
+            else:
+                delta += 1
+    return new_board
